0fc57fff7abc35517e19a3a291b7e72c9852ad16
Rohan Kapur <kapurkrohan@gmail.com>
2026-02-18 14:39:53 -0800

    feat: radar and line charts (#28)
    added a radar chart showing user accuracy by category as well as benchmark/average user values. added a line chart showing a binned trendline through speed vs accuracy as well as where the user falls on that graph.
    
    * radar chart
    
    * responsive height
    
    * typo
    
    * add category radar chart
    
    * dev script
    
    * store session in a context
    
    * tooltip on radar chart
    
    * clean up radar chart
    
    * clean up results card and fix radarchart focusing
    
    * speed vs accuracy line chart
    
    * suapbase rpc

--- src/features/study/ResultsCard.tsx
+++ src/features/study/ResultsCard.tsx
@@ -6,6 +6,16 @@ interface ResultsCardProps {
   data: ParticipantResults | null;
 }
 
+const getMessage = (percentile: number): string => {
+  if (percentile > 90) {
+    return "You're a master at catching deception!";
+  } else if (percentile > 50) {
+    return "You're hard to fool!";
+  } else {
+    return "You're starting to gain an intuition when you are being deceived.";
+  }
+};
+
 /**
  * Card that display participant results data
  * @component
@@ -14,28 +24,22 @@ export function ResultsCard({ data }: ResultsCardProps) {
   if (!data) {
     return (
       <>
-        <Paper p="xl" radius="md" withBorder w="75%">
+        <Paper p="xl" radius="md" withBorder w="100%">
           <Stack gap="lg">
             <Group justify="space-between">
-              <Text fw={500}>Total Questions Answered:</Text>
+              <Text fw={500}>Correct Answers</Text>
               <Text size="lg" fw={700}>
-                -
+                - / -
               </Text>
             </Group>
             <Group justify="space-between">
-              <Text fw={500}>Correct Answers:</Text>
+              <Text fw={500}>Accuracy:</Text>
               <Text size="lg" fw={700}>
-                -
+                -%
               </Text>
             </Group>
             <Group justify="space-between">
-              <Text fw={500}>Average Time Taken:</Text>
-              <Text size="lg" fw={700}>
-                -
-              </Text>
-            </Group>
-            <Group justify="space-between">
-              <Text fw={500}>Percentage Correct:</Text>
+              <Text fw={500}>Average Time:</Text>
               <Text size="lg" fw={700}>
                 -
               </Text>
@@ -47,40 +51,33 @@ export function ResultsCard({ data }: ResultsCardProps) {
   }
 
   return (
-    <Paper p="xl" radius="md" withBorder w="75%">
+    <Paper p="xl" radius="md" withBorder>
       <Stack gap="lg">
-        <Group justify="space-between">
-          <Text fw={500}>Total Questions Answered:</Text>
-          <Text size="lg" fw={700}>
-            {data.total_questions}
-          </Text>
-        </Group>
+        <Text size="lg">
+          You perfomed the same as or better than <b>{data.percentile}%</b> of
+          other participants
+          {data.percentile > 50 ? "!" : "."} {getMessage(data.percentile)}
+        </Text>
         <Group justify="space-between">
           <Text fw={500}>Correct Answers:</Text>
           <Text size="lg" fw={700}>
-            {data.correct_answers}
+            {data.correct_answers} / {data.total_questions}
           </Text>
         </Group>
         <Group justify="space-between">
-          <Text fw={500}>Percentage Correct:</Text>
+          <Text fw={500}>Accuracy:</Text>
           <Text size="lg" fw={700}>
             {data.accuracy_percentage}%
           </Text>
         </Group>
         <Group justify="space-between">
-          <Text fw={500}>Average Time Taken:</Text>
+          <Text fw={500}>Avg. Time:</Text>
           <Text size="lg" fw={700}>
             {`${Math.floor(data.average_time / 60000)}:${Math.floor(
               (data.average_time % 60000) / 1000,
             )
               .toString()
               .padStart(2, "0")}`}
-          </Text>
-        </Group>
-        <Group justify="space-between">
-          <Text fw={500}>Percentile:</Text>
-          <Text size="lg" fw={700}>
-            {data.percentile}%
           </Text>
         </Group>
       </Stack>
